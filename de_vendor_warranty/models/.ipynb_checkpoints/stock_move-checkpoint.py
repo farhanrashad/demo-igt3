d4c9fea6c8252a0462c6950b0ebc355b5d73dbc6
Farhan <farhan@dynexcel.com>
2021-06-21 21:38:20 +0000

    update warranty

--- de_vendor_warranty/models/.ipynb_checkpoints/stock_move-checkpoint.py
+++ de_vendor_warranty/models/.ipynb_checkpoints/stock_move-checkpoint.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 import logging
-from datetime import datetime
+#from datetime import datetime
+#import datetime as dt
+import datetime
+
 from dateutil.relativedelta import relativedelta
 import traceback
 
@@ -20,22 +23,38 @@ class StockMoveLine(models.Model):
     
     allow_warranty = fields.Boolean(string='Allow Warranty', compute="_compute_allow_warranty")
     
-    warranty_date_start = fields.Date(string='WTY Start')
-    warranty_date_end = fields.Date(string='WTY End')
-    warranty_days = fields.Integer(string='WTY Days', compute='_compute_warranty_days')
+    warranty_date_start = fields.Char(string='WTY Start')
+    warranty_date_end = fields.Char(string='WTY End')
+    warranty_days = fields.Char(string='WTY Days')
     
+    w_date = fields.Date(compute="_compute_date")
+    #warranty_date_start = fields.Date(string='WTY Start')
+    #warranty_date_end = fields.Date(string='WTY End')
+    #warranty_days = fields.Integer(string='WTY Days', compute='_compute_warranty_days')
+    
+    @api.depends('product_id','warranty_date_start','warranty_date_end')
+    def _compute_date(self):
+        date_string = ''
+        for line in self:
+            #d = dt.datetime.strptime(line.warranty_date_start, "%d-%m-%Y")
+            #d = d.date()
+            #line.w_date = d.isoformat()
+
+            #date_string = line.warranty_date_start
+            #line.w_date = (datetime.fromisoformat(date_string))
+            line.w_date = datetime.datetime.strptime(line.warranty_date_start, "%d-%m-%Y").date()
+            
     @api.depends('product_id')
     def _compute_allow_warranty(self):
         for stock in self:
-            stock.allow_warranty = self.product_id.product_tmpl_id.allow_warranty
+            stock.allow_warranty = stock.product_id.product_tmpl_id.allow_warranty
             
-    @api.depends('warranty_date_start','warranty_date_end')
+    #@api.depends('warranty_date_start','warranty_date_end')
     def _compute_warranty_days(self):
         days = 0
         for stock in self:
             if stock.warranty_date_start and stock.warranty_date_end:
-                rdelta = relativedelta(self.warranty_date_end, self.warranty_date_start)
+                rdelta = relativedelta(stock.warranty_date_end, stock.warranty_date_start)
                 days = rdelta.days
-        
             stock.warranty_days = days
     
