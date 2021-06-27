850404c185fcfc058c824614c4ef940970972f90
Farhan <farhan@dynexcel.com>
2021-06-27 12:32:56 +0000

    add depdendency

--- de_po_deviation_report/__manifest__.py
+++ de_po_deviation_report/__manifest__.py
@@ -10,7 +10,7 @@
     'sequence':1,
     'category': 'Agreement',
     'version': '14.0.0.3',
-    'depends': ['base','stock','purchase','sale'],
+    'depends': ['base','stock','purchase','report_xlsx'],
     'data': [
         'security/ir.model.access.csv',
         #'views/view_purchase_order.xml',
