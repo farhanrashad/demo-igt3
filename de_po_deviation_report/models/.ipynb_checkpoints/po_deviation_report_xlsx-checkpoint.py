6a11695a85354facf0b36dd73f0a4cc013519af6
farhan <farhan@dynexcel.com>
2021-06-27 14:11:18 +0000

    update description

--- de_po_deviation_report/models/.ipynb_checkpoints/po_deviation_report_xlsx-checkpoint.py
+++ de_po_deviation_report/models/.ipynb_checkpoints/po_deviation_report_xlsx-checkpoint.py
@@ -5,6 +5,7 @@ from odoo.exceptions import UserError
 
 class GenerateXLSXReport(models.Model):
     _name = 'report.de_po_deviation_report.po_deviation_report_xlsx'
+    _description = 'Purchase Deviation Report'
     _inherit = 'report.report_xlsx.abstract'
 
     def generate_xlsx_report(self, workbook, data, lines):
