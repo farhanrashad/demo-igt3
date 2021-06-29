31a2b73372675b96914ca0a92cb5765ac344a3a9
Farhan <farhan@dynexcel.com>
2021-06-29 09:44:29 +0000

    update jouranl entry

--- de_custom_journal_entry/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_custom_journal_entry/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,7 +16,7 @@
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Accounting',
-    'version': '14.0.5.1',
+    'version': '14.0.5.2',
     
     'depends': ['de_purchase_subscription','account', 'analytic', 'fleet', 'de_project_planning','project','stock','purchase_requisition','purchase','de_travel_request'],
 
