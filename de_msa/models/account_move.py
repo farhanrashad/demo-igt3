efa7bcbb13262bd02dbca1f207f5ff1ffc85dd0f
dynexcel-atif <m.atif@dynexcel.co>
2021-06-29 07:30:22 +0000

    updated msa

--- de_msa/models/account_move.py
+++ de_msa/models/account_move.py
@@ -8,6 +8,7 @@ from odoo.exceptions import ValidationError, UserError
     
 class AccountMoveInh(models.Model):
     _inherit = 'account.move'
+    _description = 'Account Move MSA'
     
     msa_id = fields.Many2one('master.service.agreement', string='MSA')
     category = fields.Selection([('Capex','Capex'),('Opex','Opex')], default='Capex')
