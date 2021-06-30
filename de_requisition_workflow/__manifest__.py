4368e24479a3be8bb3cfeb7cc6a13a8f3793def2
Farhan Rashad <farhan@dynexcel.com>
2021-07-01 04:08:58 +0500

    add security

--- de_requisition_workflow/__manifest__.py
+++ de_requisition_workflow/__manifest__.py
@@ -22,7 +22,7 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/ir.model.access.csv',
         'views/requisition_views.xml',
         'views/templates.xml',
     ],
