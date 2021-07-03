21d6631ee87b029e4c52697966d832ebada81934
dynexcel-ddc1 <ddc1@dynexce.co>
2021-07-03 10:23:41 +0500

    update batch payslip report

---
+++ de_payslip_batch_report/models/__init__.py
@@ -0,0 +1,2 @@
+from . import payslip_batch
+from . import payslip_batch_report_xlsx
---
+++ de_payslip_batch_report/wizards/.ipynb_checkpoints/payslip_batch-checkpoint.py
@@ -0,0 +1,56 @@
+import json
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+from datetime import datetime
+import base64
+
+
+
+class PayslipBatch(models.TransientModel):
+    _name = "pay.slip.batch"
+    _description = "Payslip Batch Wizard"
+
+    contract_type = fields.Selection([('local', 'Local'),('expats', 'Expats')], string='Contract Type')
+    doe = fields.Date(string='Date of Execution')
+    debit_ac_no = fields.Float(string='Debit A/C No.')
+    currency = fields.Many2one('res.currency', string='Currency')
+    batch_id = fields.Many2one('hr.payslip.run')
+    date_today = fields.Date(default=datetime.today())
+    
+    def print_report(self):
+        print('ID: ', self.id)
+        datas = {
+            'contract_type': self.contract_type,
+            'doe': self.doe,
+            'debit_ac_no': self.debit_ac_no,
+            'currency': self.currency,
+            'id': self.batch_id.id,
+        }
+        return self.env.ref('de_payslip_batch_report.payslip_batch_report').report_action(self, data=datas)
+    
+    
+    
+    def print_txt_report(self):
+        data_val = ''
+        vals = ''
+        filename = "batch.txt"
+        file_ = open(filename + str(), 'w')
+        payslips = self.env['hr.payslip'].search([('payslip_run_id', '=', self.batch_id.id)])
+        for payslip in payslips:
+            line = 'This'
+            data_val = str(line)
+        file_.write(data_val)
+        file_.close()
+        
+        with open('batch.txt') as f:
+            attachment_vals = {
+                    'name': filename,
+                    'type': 'binary',
+                    'datas': base64.b64decode(f),
+                    'res_id': self.batch_id.id,
+                    'res_name': self.batch_id.name,
+                    'res_model': 'hr.payslip.run',
+            }
+            attach_file = self.env['ir.attachment'].create(attachment_vals)
+
