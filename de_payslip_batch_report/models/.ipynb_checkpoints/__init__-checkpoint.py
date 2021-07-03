b4865a61866b708493c013750b6410398c6dd9e3
dynexcel-atif <m.atif@dynexcel.co>
2021-07-03 11:49:35 +0000

    update batch

--- de_payslip_batch_report/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ de_payslip_batch_report/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,2 +1,2 @@
-from . import payslip_batch
 from . import payslip_batch_report_xlsx
+from . import batch_payroll
--- de_payslip_batch_report/wizards/payslip_batch.py
+++ de_payslip_batch_report/wizards/payslip_batch.py
@@ -38,20 +38,30 @@ class PayslipBatch(models.TransientModel):
         file_ = open(filename + str(), 'w')
         payslips = self.env['hr.payslip'].search([('payslip_run_id', '=', self.batch_id.id)])
         for payslip in payslips:
-            line = 'This'
-            data_val = str(line)
+             line = 'TT' + '  ' + str(payslip.number) + '        ' + str(self.debit_ac_no) + '  ' + str(self.currency.name) + str(self.date_today) + '   ' + str(payslip.net_wage) + '         ' + str(payslip.employee_id.bank_account_id.acc_number) + '                    ' + str(payslip.employee_id.address_id.street) + str(payslip.employee_id.address_id.street2) + str(payslip.employee_id.address_id.city) + ' ' + str(payslip.employee_id.address_id.country_id.name) + '                              ' + str(payslip.employee_id.bank_account_id.bank_id.name) + '                      ' + str(payslip.employee_id.bank_account_id.bank_id.street) + "\n" + str(payslip.name) + "\n" + str(payslip.employee_id.work_email)
+        data_val = str(line)
         file_.write(data_val)
         file_.close()
         
         with open('batch.txt') as f:
-            raise UserError(f)
+            read_file = f.read()
             attachment_vals = {
                     'name': filename,
                     'type': 'binary',
-                    'datas': base64.b64decode(f.read()),
+                    'datas': base64.b64encode(read_file.encode('utf8')),
                     'res_id': self.batch_id.id,
                     'res_name': self.batch_id.name,
                     'res_model': 'hr.payslip.run',
             }
             attach_file = self.env['ir.attachment'].create(attachment_vals)
+            download_url = '/web/content/' + str(attach_file.id) + '?download=True'
+            base_url = self.env['ir.config_parameter'].get_param('web.base.url')
+#             Return so it will download in your system
+            return {
+                    "type": "ir.actions.act_url",
+                    "url": str(base_url)  +  str(download_url),
+                    "target": "new",
+            }
+        
+        
 
