215a4c504b75074486421819ac81f87596fc1c3b
dynexcel-ddc1 <ddc1@dynexce.co>
2021-07-09 21:22:47 +0500

    update manifest

--- de_hr_overtime/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_hr_overtime/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -1,28 +1,24 @@
 # -*- coding: utf-8 -*-
+###################################################################################
 {
-    'name': "Employee Overtime",
-
-    'summary': """
-        Manage Employee Overtime""",
-
+    'name': 'Employee Overtime',
+    'version': '14.0.0.5',
+    'summary': 'Manage Employee Overtime',
     'description': """
         Helps you to manage Employee Overtime.
-    """,
-
-    'author': "Dynexcel",
-    'website': "httsp://www.dynexcel.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
+        """,
     'category': 'Human Resources',
-    'version': '14.0.0.5',
-
-    # any module necessary for this one to work correctly
-    'depends': ['base','hr', 'hr_contract', 'hr_holidays','hr_payroll', 'hr_attendance','project','hr_recruitment'],
-
-    # always loaded
+    'author': "Dynexcel",
+    'live_test_url': 'https://youtu.be/lOQCTCxrUKs',
+    'company': 'Dynexcel',
+    'maintainer': 'Dynexcel',
+    'website': "https://www.dynexcel.com",
+    'depends': [
+        'base','hr', 'hr_contract', 'hr_holidays','hr_payroll', 'hr_attendance','project','hr_recruitment'
+    ],
+    
     'data': [
+
         'security/ir.model.access.csv',
         'data/data.xml',
         'views/overtime_menu.xml',
@@ -32,11 +28,9 @@
         'views/overtime_request_views.xml',
         'views/hr_payslip_views.xml',
         'views/hr_employee_views.xml',
+        
     ],
-    # only loaded in demonstration mode
-    'demo': [
-       'data/hr_overtime_demo.xml'
-    ],
+    #'demo': ['data/hr_overtime_demo.xml'],
     'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
