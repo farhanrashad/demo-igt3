1b2840b57fa9a44dda3979a64558c9b3769d57b8
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-07-09 05:25:47 +0000

    update shift

--- de_employee_shift/models/hr_shift_payroll.py
+++ de_employee_shift/models/hr_shift_payroll.py
@@ -212,7 +212,7 @@ class Calendar(models.Model):
         ('evening', 'Evening'),
         ('night', 'Night'),
         
-    ], string='Shift Type',copy=False, required=True)
+    ], string='Shift Type',copy=True, required=True)
     
     
     def _check_overlap(self, attendance_ids):
