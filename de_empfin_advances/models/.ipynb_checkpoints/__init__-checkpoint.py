43a1742fe7815ec2125e02b6f1c08961887ce018
Farhan <farhan@dynexcel.com>
2021-07-09 10:54:20 +0000

    udpate advances

--- de_empfin_advances/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ de_empfin_advances/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from . import hr_salary_advance
 from . import hr_salary_structure
-from . import hr_advance_payslip
+#from . import hr_advance_payslip
 from . import account
 from . import product
 from . import hr_advance_type
