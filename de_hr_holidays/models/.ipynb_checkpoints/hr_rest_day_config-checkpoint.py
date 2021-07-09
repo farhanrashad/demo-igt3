ddf30da4ede3b6136dee8cee05ab1be2f5141e56
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-07-09 04:57:42 +0000

    update holidays

--- de_hr_holidays/models/.ipynb_checkpoints/hr_rest_day_config-checkpoint.py
+++ de_hr_holidays/models/.ipynb_checkpoints/hr_rest_day_config-checkpoint.py
@@ -5,8 +5,8 @@ class HrRestDayConfig(models.Model):
     _name = 'hr.rest.day.config'
     
     name = fields.Char(string="Name")
-    leave_type_id = fields.Many2one('hr.leave.type', string="Timeoff Type", required=True)
-    company_id = fields.Many2one('res.company', string="Company", required=False)
+    leave_type_id = fields.Many2one('hr.leave.type', string="Timeoff Type", required=True, domain="['|',('company_id','=',company_id),('company_id','=',False)]")
+    company_id = fields.Many2one('res.company', string="Company", required=False, default=lambda self: self.env.company)
 
     
     
