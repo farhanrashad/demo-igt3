1d8ed69e21fbbcebe7f66c166504426bcbdb060f
Farhan <farhan@dynexcel.com>
2021-07-17 13:39:56 +0000

    update journal eb history

--- de_custom_journal_entry_eb_history/models/.ipynb_checkpoints/custom_entry-checkpoint.py
+++ de_custom_journal_entry_eb_history/models/.ipynb_checkpoints/custom_entry-checkpoint.py
@@ -12,7 +12,48 @@ from lxml import etree
 class CustomEntry(models.Model):
     _inherit = 'account.custom.entry'
     
+    has_advanced = fields.Selection(related="custom_entry_type_id.has_advanced")
+    has_electricity = fields.Selection(related="custom_entry_type_id.has_electricity")
+    
+    amount_advanced_total = fields.Float('Total Advanced', compute='_amount_all')
+    
     custom_entry_eb_history_line = fields.One2many('account.custom.entry.ebh.line', 'custom_entry_id', string='Entry Line for Electricity Bill', copy=True, auto_join=True,)
+    
+class CustomEntryLine(models.Model):
+    _inherit = 'account.custom.entry.line'
+    
+    advance_subtotal = fields.Monetary(string='Adv. Subtotal', store=True)
+    
+    #has electricity
+    e_paid_to = fields.Selection([
+        ('govt', 'Government'),
+        ('private', 'Private')],
+        string='Paid To')
+    date_bill_from = fields.Date(string='Date From', )
+    date_bill_to = fields.Date(string='Date To', )
+    amount_advanced = fields.Float(string='Forecast', help='Advanced Amount')
+    meter_number = fields.Char(string='Meter')
+    opening_reading = fields.Integer(string='Opening Reading', help='Opening reading of meter')
+    closing_reading = fields.Integer(string='Closing Reading', help='Closing Reading of meter')
+    additional_unit = fields.Integer(string='Additional Units')
+    total_unit = fields.Integer(string='Total Unit', compute='_compute_total_units', store=True)
+    maintainence_fee = fields.Float(string='Mnt. Fees', help='Maintenance Fees')
+    hp_fee = fields.Float(string='HP Fee', help='Horsepower Fees')
+    KHW_charges = fields.Float(string='KHW')
+    actual_KHW_charges = fields.Float(string='Actual KHW')
+    other_charges = fields.Float(string='Other Charges')
+    amount_total_electricity = fields.Float(string='Total', compute='_compute_total_electricity_amount', store=True)
+    
+    @api.depends('opening_reading','closing_reading','additional_unit')
+    def _compute_total_units(self):
+        for rec in self:
+            rec.total_unit = (rec.closing_reading - rec.opening_reading) + rec.additional_unit
+
+    @api.depends('maintainence_fee','hp_fee','KHW_charges','other_charges')
+    def _compute_total_electricity_amount(self):
+        for rec in self:
+            rec.amount_total_electricity = rec.maintainence_fee + rec.hp_fee + rec.KHW_charges + rec.other_charges
+            
     
 class CustomEntryLine(models.Model):
     _name = 'account.custom.entry.ebh.line'
