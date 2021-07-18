311a6c5e1816ad28ebd24702d73f20bdea423ef8
Farhan <farhan@dynexcel.com>
2021-07-18 10:30:44 +0000

    update journal entry hotel

--- de_custom_journal_entry_hotel/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ de_custom_journal_entry_hotel/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -16,7 +16,7 @@
     'version': '14.0.0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['de_custom_journal_entry'],
+    'depends': ['de_custom_journal_entry','de_travel_request'],
 
     # always loaded
     'data': [
