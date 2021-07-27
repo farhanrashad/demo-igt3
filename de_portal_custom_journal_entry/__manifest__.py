a2912328ce1edb04d6eb9c04217a0bb7186d1b1a
Farhan Rashad <farhan@dynexcel.com>
2021-07-28 02:21:36 +0500

    update custom entry

--- de_portal_custom_journal_entry/__manifest__.py
+++ de_portal_custom_journal_entry/__manifest__.py
@@ -21,7 +21,7 @@
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Portal',
-    'version': '14.0.0.1',
+    'version': '14.0.0.2',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'portal',  'rating', 'resource', 'web', 'web_tour', 'digest', 'de_custom_journal_entry', 'de_custom_journal_entry_import'],
--- de_portal_custom_journal_entry/models/.ipynb_checkpoints/custom_entry-checkpoint.py
+++ de_portal_custom_journal_entry/models/.ipynb_checkpoints/custom_entry-checkpoint.py
@@ -6,8 +6,8 @@ from odoo import models, fields, api, _
 class CustumEntry(models.Model):
     _inherit = 'account.custom.entry'    
     
-    correction_reason = fields.Char(string='Correction Reason')
-    
+    correction_reason = fields.Char(string='Correction Reason', tracking=True)
+    allow_correction = fields.Boolean(string='Allow Correction')
     
     def action_correction_entry(self):
         for rec in self:
@@ -23,11 +23,6 @@ class CustumEntry(models.Model):
             'target': 'new',
             'context': {'default_custom_entry_id': self.id},
         }
-    
-    
-    
-    
-    
        
 class CustumEntryline(models.Model):
     _inherit = 'account.custom.entry.line'
@@ -35,3 +30,6 @@ class CustumEntryline(models.Model):
 
 class CustumEntrystage(models.Model):
     _inherit = 'account.custom.entry.stage'    
+
+class MailMessage(models.Model):
+    _inherit = 'mail.message'    
