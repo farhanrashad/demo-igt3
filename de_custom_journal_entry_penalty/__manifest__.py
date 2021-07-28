0d60f26a1095579778b09b3caa74d3ec575e5093
Farhan Rashad <farhan@dynexcel.com>
2021-07-28 23:52:34 +0500

    update penalties

--- de_custom_journal_entry_penalty/__manifest__.py
+++ de_custom_journal_entry_penalty/__manifest__.py
@@ -16,7 +16,7 @@
     'author': "Dynexcel",
     'website': "https://www.dynexcel.com",
     'category': 'Accounting',
-    'version': '14.0.0.4',
+    'version': '14.0.0.5',
 
     # any module necessary for this one to work correctly
     'depends': ['de_custom_journal_entry'],
@@ -26,10 +26,8 @@
         'security/penalty_security.xml',
         'security/ir.model.access.csv',
         'views/penalty_menu.xml',
-        'views/custom_entry_type_views.xml',
-        #'views/penalty_stage_views.xml',
         'views/penalty_config_views.xml',
-        #'views/account_move_views.xml',
-        #'views/penalty_entry_views.xml',
+        'views/custom_entry_type_views.xml',        
+        'views/custom_entry_views.xml',
     ],
 }
