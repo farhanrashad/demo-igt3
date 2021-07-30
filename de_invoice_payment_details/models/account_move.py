7b52c8f889f497721dcbb1f48a468a1031c236ee
farhan <farhan@dynexcel.com>
2021-07-30 01:47:31 +0000

    update payment details

--- de_invoice_payment_details/models/account_move.py
+++ de_invoice_payment_details/models/account_move.py
@@ -10,13 +10,15 @@ from odoo import models, fields, api
 class AccountMove(models.Model):
     _inherit = 'account.move'
     
-    #payment_ids = fields.Many2one('account.payment', string='Payments', compute='_compute_payments')
-    payment_ids = fields.Char(string='Payments', compute='_compute_payments')
+    payment_ids = fields.Char(string='Payments')
+    invoice_payment_ids = fields.Many2many('account.payment',  string='Payments', compute='_compute_payments')
     
     def _compute_payments(self):
         invoice_payments_widget = json.loads(self.invoice_payments_widget)
         payment_ids = []
-        for item in invoice_payments_widget["content"]:
-            payment_ids.append(item["account_payment_id"])
+        if invoice_payments_widget:
+            for item in invoice_payments_widget["content"]:
+                payment_ids.append(item["account_payment_id"])
         #for payment in payment_ids:
-        self.payment_ids = payment_ids
+        self.invoice_payment_ids = payment_ids
+        
