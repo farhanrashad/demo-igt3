e84c123f771dca86ea7ee3fcb37f9d67b4909053
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-08-03 05:09:02 +0000

    update payslip batch

--- de_payslip_batch_report/wizards/.ipynb_checkpoints/payslip_batch-checkpoint.py
+++ de_payslip_batch_report/wizards/.ipynb_checkpoints/payslip_batch-checkpoint.py
@@ -39,10 +39,19 @@ class PayslipBatch(models.TransientModel):
         payslips = self.env['hr.payslip'].search([('payslip_run_id', '=', self.batch_id.id)])
         file_data = '' 
         for payslip in payslips:
-            self.env.cr.execute("""select rpad(p.number,16,' '), rpad(ps.debit_ac_no,16,' ')  from pay_slip_batch as ps 
-            left join hr_payslip as p on ps.batch_id=p.payslip_run_id            
+            self.env.cr.execute("""select rpad(concat('',p.number),16,' '), rpad(concat('',ps.debit_ac_no),14,' '), rpad(concat('',c.name,ps.date_today),14,' '),rpad(concat('',p.normal_wage),15,' '), rpad(concat('NNOUR',bank.acc_number),40,' '),rpad(concat('',c.name,bank.acc_holder_name),38,' '),rpad(concat('',partner.street,partner.street2,partner.city),70,' '),rpad(concat('',country.name),35,' '),rpad(concat(bbank.name,bbank.street),105,' '),rpad(concat('',bcountry.name),186,' '),rpad(concat('',p.name),350,' '),rpad(concat('',emp.work_email),538,' '),rpad(concat('',bbank.bic),503,' ')
+            from pay_slip_batch as ps 
+            left join hr_payslip as p on ps.batch_id=p.payslip_run_id
+            left join hr_employee as emp on p.employee_id=emp.id
+            left join res_partner as partner on emp.address_home_id=partner.id
+            left join res_country as country on partner.country_id=country.id
+            left join res_partner_bank as bank on emp.bank_account_id=bank.id
+            left join res_bank as bbank on bank.bank_id=bbank.id
+            left join res_country as bcountry on bbank.country=bcountry.id
+            left join res_currency as c on ps.currency=c.id
             where p.id=%s and ps.id=%s""" % (payslip.id, self.id))                         
             payslip_vals = self.env.cr.fetchall()
+            count = 0
             for line_vals in  payslip_vals:
                 slip_line = ''
                 for payslip_line in line_vals:
