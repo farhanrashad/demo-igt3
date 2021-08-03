5b7e8973abddfdedfbbb2cb4f05ab4578be6659c
Farhan Rashad <farhan@dynexcel.com>
2021-08-03 05:59:02 +0500

    make storeable purchase order

--- de_purchase_tasks/__manifest__.py
+++ de_purchase_tasks/__manifest__.py
@@ -19,7 +19,7 @@
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Project',
-    'version': '14.0.1.2',
+    'version': '14.0.1.3',
 
     # any module necessary for this one to work correctly
     'depends': ['de_purchase_requisition_type','de_project_planning','de_stock_billing','de_project_task_workflow'],
