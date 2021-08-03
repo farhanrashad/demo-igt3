5f975092e381f40d8c1dfee0f80c593bf5015bdc
dynexcel-ddc1 <ddc1@dynexce.co>
2021-08-03 10:20:17 +0500

    update payslip batch txt report

--- de_payslip_batch_report/wizards/.ipynb_checkpoints/payslip_batch-checkpoint.py
+++ de_payslip_batch_report/wizards/.ipynb_checkpoints/payslip_batch-checkpoint.py
@@ -13,7 +13,7 @@ class PayslipBatch(models.TransientModel):
 
     contract_type = fields.Selection([('local', 'Local'),('expats', 'Expats')], string='Contract Type')
     doe = fields.Date(string='Date of Execution')
-    debit_ac_no = fields.Float(string='Debit A/C No.')
+    debit_ac_no = fields.Char(string='Debit A/C No.')
     currency = fields.Many2one('res.currency', string='Currency')
     batch_id = fields.Many2one('hr.payslip.run')
     date_today = fields.Date(default=datetime.today())
@@ -37,10 +37,27 @@ class PayslipBatch(models.TransientModel):
         filename = "batch.txt"
         file_ = open(filename + str(), 'w')
         payslips = self.env['hr.payslip'].search([('payslip_run_id', '=', self.batch_id.id)])
-        line = '' 
+        file_data = '' 
         for payslip in payslips:
-            line += 'TT' + '  ' + str(payslip.number) + '        ' + str(round(self.debit_ac_no)) + '  ' + str(self.currency.name) + str(self.date_today.strftime("%Y%m%d")) + '   ' + str(payslip.net_wage) + '         ' + ' NNOUR'+str(payslip.employee_id.bank_account_id.acc_number) + '                    ' + str(self.currency.name)+str(payslip.employee_id.bank_account_id.acc_holder_name)+'     '+str(payslip.employee_id.address_id.street)+ '  '+ str(payslip.employee_id.address_id.street2) + ' '+ str(payslip.employee_id.address_id.city) + ' ' + str(payslip.employee_id.address_id.country_id.name) + '                              ' + str(payslip.employee_id.bank_account_id.bank_id.name)  + '     ' + str(payslip.employee_id.bank_account_id.bank_id.street) + ' ' + str(payslip.name) + str(payslip.employee_id.work_email) +"\n"
-        data_val = str(line)
+            self.env.cr.execute("""select rpad(concat('',p.number),16,' '), rpad(concat('',ps.debit_ac_no),14,' '), rpad(concat('',c.name,ps.date_today),14,' '),rpad(concat('',p.normal_wage),15,' '), rpad(concat('NNOUR',bank.acc_number),40,' '),rpad(concat('',c.name,bank.acc_holder_name),38,' '),rpad(concat('',partner.street,partner.street2,partner.city),70,' '),rpad(concat('',country.name),35,' '),rpad(concat(bbank.name,bbank.street),105,' '),rpad(concat('',bcountry.name),186,' '),rpad(concat('',p.name),350,' '),rpad(concat('',emp.work_email),538,' '),rpad(concat('',bbank.bic),503,' ')
+            from pay_slip_batch as ps 
+            left join hr_payslip as p on ps.batch_id=p.payslip_run_id
+            left join hr_employee as emp on p.employee_id=emp.id
+            left join res_partner as partner on emp.address_home_id=partner.id
+            left join res_country as country on partner.country_id=country.id
+            left join res_partner_bank as bank on emp.bank_account_id=bank.id
+            left join res_bank as bbank on bank.bank_id=bbank.id
+            left join res_country as bcountry on bbank.country=bcountry.id
+            left join res_currency as c on ps.currency=c.id
+            where p.id=%s and ps.id=%s""" % (payslip.id, self.id))                         
+            payslip_vals = self.env.cr.fetchall()
+            count = 0
+            for line_vals in  payslip_vals:
+                slip_line = ''
+                for payslip_line in line_vals:
+                    slip_line += payslip_line    
+                file_data += 'TT' + '  ' + str(slip_line) + "\n"
+        data_val = str(file_data)
         file_.write(data_val)
         file_.write("\n")
         file_.close()
