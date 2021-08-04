8ccfab62b1ef7266bf21fda65834fc74a644a3f1
dynexcel-ddc1 <ddc1@dynexce.co>
2021-08-04 13:44:57 +0500

    update de_invocie_payment_details

--- de_invoice_payment_details/models/account_move.py
+++ de_invoice_payment_details/models/account_move.py
@@ -14,11 +14,12 @@ class AccountMove(models.Model):
     invoice_payment_ids = fields.Many2many('account.payment',  string='Payments', compute='_compute_payments')
     
     def _compute_payments(self):
-        invoice_payments_widget = json.loads(self.invoice_payments_widget)
-        payment_ids = []
-        if invoice_payments_widget:
-            for item in invoice_payments_widget["content"]:
-                payment_ids.append(item["account_payment_id"])
-        #for payment in payment_ids:
-        self.invoice_payment_ids = payment_ids
+        for line in self:
+            invoice_payments_widget = json.loads(line.invoice_payments_widget)
+            payment_ids = []
+            if invoice_payments_widget:
+                for item in invoice_payments_widget["content"]:
+                    payment_ids.append(item["account_payment_id"])
+            #for payment in payment_ids:
+            line.invoice_payment_ids = payment_ids
         
