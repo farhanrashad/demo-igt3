ef336096cc7ba8d69504cbdc31ccff87dab45526
Farhan <farhan@dynexcel.com>
2021-08-05 19:59:59 +0000

    update operations

--- de_operations/models/.ipynb_checkpoints/__init__-checkpoint.py
+++ de_operations/models/.ipynb_checkpoints/__init__-checkpoint.py
@@ -5,7 +5,7 @@ from . import sale_subscription_type
 #from . import sale_subscription
 from . import account_journal
 from . import account_move
-from . import purchase_subscription
-from . import purchase_subscription_deductions
+#from . import purchase_subscription
+#from . import purchase_subscription_deductions
 from . import project
 #from . import custom_entry
