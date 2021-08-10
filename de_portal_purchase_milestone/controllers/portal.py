411da1442943400f88f6c9d8059e5e3cdc769c91
dynexcel-ramzan <m.ramzan@dynexcel.co>
2021-08-10 16:13:11 +0000

    update portal mile stone

--- de_portal_purchase_milestone/controllers/portal.py
+++ de_portal_purchase_milestone/controllers/portal.py
@@ -12,6 +12,165 @@ from odoo.addons.portal.controllers.portal import CustomerPortal, pager as porta
 from odoo.tools import groupby as groupbyelem
 
 from odoo.osv.expression import OR
+import werkzeug
+from werkzeug import urls
+from werkzeug.exceptions import NotFound, Forbidden
+
+from odoo import http, _
+from odoo.http import request
+from odoo.osv import expression
+from odoo.tools import consteq, plaintext2html
+from odoo.addons.mail.controllers.main import MailController
+from odoo.addons.portal.controllers.portal import CustomerPortal
+from odoo.exceptions import AccessError, MissingError, UserError
+
+
+def _check_special_access(res_model, res_id, token='', _hash='', pid=False):
+    record = request.env[res_model].browse(res_id).sudo()
+    if token:  # Token Case: token is the global one of the document
+        token_field = request.env[res_model]._mail_post_token_field
+        return (token and record and consteq(record[token_field], token))
+    elif _hash and pid:  # Signed Token Case: hash implies token is signed by partner pid
+        return consteq(_hash, record._sign_token(pid))
+    else:
+        raise Forbidden()
+
+
+def _message_post_helper(res_model, res_id, message, token='', _hash=False, pid=False, nosubscribe=True, **kw):
+    """ Generic chatter function, allowing to write on *any* object that inherits mail.thread. We
+        distinguish 2 cases:
+            1/ If a token is specified, all logged in users will be able to write a message regardless
+            of access rights; if the user is the public user, the message will be posted under the name
+            of the partner_id of the object (or the public user if there is no partner_id on the object).
+
+            2/ If a signed token is specified (`hash`) and also a partner_id (`pid`), all post message will
+            be done under the name of the partner_id (as it is signed). This should be used to avoid leaking
+            token to all users.
+
+        Required parameters
+        :param string res_model: model name of the object
+        :param int res_id: id of the object
+        :param string message: content of the message
+
+        Optional keywords arguments:
+        :param string token: access token if the object's model uses some kind of public access
+                             using tokens (usually a uuid4) to bypass access rules
+        :param string hash: signed token by a partner if model uses some token field to bypass access right
+                            post messages.
+        :param string pid: identifier of the res.partner used to sign the hash
+        :param bool nosubscribe: set False if you want the partner to be set as follower of the object when posting (default to True)
+
+        The rest of the kwargs are passed on to message_post()
+    """
+    record = request.env[res_model].browse(res_id)
+
+    # check if user can post with special token/signed token. The "else" will try to post message with the
+    # current user access rights (_mail_post_access use case).
+    if token or (_hash and pid):
+        pid = int(pid) if pid else False
+        if _check_special_access(res_model, res_id, token=token, _hash=_hash, pid=pid):
+            record = record.sudo()
+        else:
+            raise Forbidden()
+
+    # deduce author of message
+    author_id = request.env.user.partner_id.id if request.env.user.partner_id else False
+
+    # Token Case: author is document customer (if not logged) or itself even if user has not the access
+    if token:
+        if request.env.user._is_public():
+            # TODO : After adding the pid and sign_token in access_url when send invoice by email, remove this line
+            # TODO : Author must be Public User (to rename to 'Anonymous')
+            author_id = record.partner_id.id if hasattr(record, 'partner_id') and record.partner_id.id else author_id
+        else:
+            if not author_id:
+                raise NotFound()
+    # Signed Token Case: author_id is forced
+    elif _hash and pid:
+        author_id = pid
+
+    email_from = None
+    if author_id and 'email_from' not in kw:
+        partner = request.env['res.partner'].sudo().browse(author_id)
+        email_from = partner.email_formatted if partner.email else None
+
+    message_post_args = dict(
+        body=message,
+        message_type=kw.pop('message_type', "comment"),
+        subtype_xmlid=kw.pop('subtype_xmlid', "mail.mt_comment"),
+        author_id=author_id,
+        **kw
+    )
+
+    # This is necessary as mail.message checks the presence
+    # of the key to compute its default email from
+    if email_from:
+        message_post_args['email_from'] = email_from
+
+    return record.with_context(mail_create_nosubscribe=nosubscribe).message_post(**message_post_args)
+
+
+
+class PortalChatter(http.Controller):
+    
+    def _portal_post_filter_params(self):
+        return ['token', 'hash', 'pid']
+
+    def _portal_post_check_attachments(self, attachment_ids, attachment_tokens):
+        if len(attachment_tokens) != len(attachment_ids):
+            raise UserError(_("An access token must be provided for each attachment."))
+        for (attachment_id, access_token) in zip(attachment_ids, attachment_tokens):
+            try:
+                CustomerPortal._document_check_access(self, 'ir.attachment', attachment_id, access_token)
+            except (AccessError, MissingError):
+                raise UserError(_("The attachment %s does not exist or you do not have the rights to access it.", attachment_id))
+
+    @http.route(['/mail/chatter_post'], type='http', methods=['POST'], auth='public', website=True)
+    def portal_chatter_post(self, res_model, res_id, message, redirect=None, attachment_ids='', attachment_tokens='', **kw):
+        """Create a new `mail.message` with the given `message` and/or
+        `attachment_ids` and redirect the user to the newly created message.
+
+        The message will be associated to the record `res_id` of the model
+        `res_model`. The user must have access rights on this target document or
+        must provide valid identifiers through `kw`. See `_message_post_helper`.
+        """
+        url = redirect or (request.httprequest.referrer and request.httprequest.referrer + "#discussion") or '/my'
+
+        res_id = int(res_id)
+
+        attachment_ids = [int(attachment_id) for attachment_id in attachment_ids.split(',') if attachment_id]
+        attachment_tokens = [attachment_token for attachment_token in attachment_tokens.split(',') if attachment_token]
+        self._portal_post_check_attachments(attachment_ids, attachment_tokens)
+
+        if message or attachment_ids:
+            # message is received in plaintext and saved in html
+            if message:
+                message = plaintext2html(message)
+            post_values = {
+                'res_model': res_model,
+                'res_id': res_id,
+                'message': message,
+                'send_after_commit': False,
+                'attachment_ids': False,  # will be added afterward
+            }
+            post_values.update((fname, kw.get(fname)) for fname in self._portal_post_filter_params())
+            message = _message_post_helper(**post_values)
+
+            if attachment_ids:
+                # sudo write the attachment to bypass the read access
+                # verification in mail message
+                record = request.env[res_model].browse(res_id)
+                message_values = {'res_id': res_id, 'model': res_model}
+                attachments = record._message_post_process_attachments([], attachment_ids, message_values)
+
+                if attachments.get('attachment_ids'):
+                    if record.purchase_id:
+                        record.update({
+                           'is_purchase_attachment': True
+                       }) 
+                    message.sudo().write(attachments)
+
+        return request.redirect(url)
 
 
 
@@ -19,25 +178,25 @@ class CustomerPortal(CustomerPortal):
 
     def _prepare_home_portal_values(self, counters):
         values = super()._prepare_home_portal_values(counters)
-        if 'hoto_count' in counters:
+        if 'milestone_count' in counters:
             active_user = request.env['res.users'].search([('id','=',http.request.env.context.get('uid'))])    
-            values['hoto_count'] = request.env['project.task'].search_count([('partner_id','=', active_user.partner_id.id)])
+            values['milestone_count'] = request.env['project.task'].search_count([('partner_id','=', active_user.partner_id.id)])
         return values
 
    
     # ------------------------------------------------------------
-    # My Task
+    #  Task
     # ------------------------------------------------------------
-    def _hoto_site_task_get_page_view_values(self, task, access_token, **kwargs):
+    def _milestone_task_get_page_view_values(self, task, access_token, **kwargs):
         values = {
-            'page_name': 'Hoto task',
+            'page_name': 'milestone_task',
             'task': task,
             'user': request.env.user
         }
-        return self._get_page_view_values(task, access_token, values, 'hoto_tasks_history', False, **kwargs)
+        return self._get_page_view_values(task, access_token, values, 'milestone_tasks_history', False, **kwargs)
 
-    @http.route(['/hoto/tasks', '/hoto/tasks/page/<int:page>'], type='http', auth="user", website=True)
-    def portal_hoto_tasks(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None, search_in='content', groupby=None, **kw):
+    @http.route(['/purchase/milestone/tasks', '/purchase/milestone/tasks/page/<int:page>'], type='http', auth="user", website=True)
+    def portal_purchase_milestone_tasks(self, page=1, date_begin=None, date_end=None, sortby=None, filterby=None, search=None, search_in='content', groupby=None, **kw):
         values = self._prepare_portal_layout_values()
         searchbar_sortings = {
             'date': {'label': _('Newest'), 'order': 'create_date desc'},
@@ -64,7 +223,7 @@ class CustomerPortal(CustomerPortal):
         }
 
         # extends filterby criteria with project the customer has access to
-        projects = request.env['project.project'].search([('name','=','HOTO')])
+        projects = request.env['project.project'].search([])
         for project in projects:
             searchbar_filters.update({
                 str(project.id): {'label': project.name, 'domain': [('project_id', '=', project.id)]}
@@ -72,14 +231,7 @@ class CustomerPortal(CustomerPortal):
 
         # extends filterby criteria with project (criteria name is the project id)
         # Note: portal users can't view projects they don't follow
-        project_groups = request.env['project.task'].read_group([('project_id', '!=', projects.id)],
-                                                                ['project_id'], ['project_id'])
-        for group in project_groups:
-            proj_id = group['project_id'][0] if group['project_id'] else False
-            proj_name = group['project_id'][1] if group['project_id'] else _('Others')
-            searchbar_filters.update({
-                str(proj_id): {'label': proj_name, 'domain': [('project_id', '=', proj_id)]}
-            })
+
 
         # default sort by value
         if not sortby:
@@ -116,12 +268,12 @@ class CustomerPortal(CustomerPortal):
         active_user = request.env['res.users'].search([('id','=',http.request.env.context.get('uid'))])    
         domain += [('partner_id', '=', active_user.partner_id.id)]
         # task count
-        task_count = request.env['project.task'].search_count(domain)
+        milestone_count = request.env['project.task'].search_count(domain)
         # pager
         pager = portal_pager(
-            url="/hoto/tasks",
+            url="/purchase/milestone/tasks",
             url_args={'date_begin': date_begin, 'date_end': date_end, 'sortby': sortby, 'filterby': filterby, 'groupby': groupby, 'search_in': search_in, 'search': search},
-            total=task_count,
+            total=milestone_count,
             page=page,
             step=self._items_per_page
         )
@@ -132,7 +284,7 @@ class CustomerPortal(CustomerPortal):
             order = "stage_id, %s" % order  # force sort on stage first to group by stage in view
 
         tasks = request.env['project.task'].search(domain, order=order, limit=self._items_per_page, offset=pager['offset'])
-        request.session['hoto_tasks_history'] = tasks.ids[:100]
+        request.session['milestone_tasks_history'] = tasks.ids[:100]
 
         
         grouped_tasks = [tasks]
@@ -141,8 +293,8 @@ class CustomerPortal(CustomerPortal):
             'date': date_begin,
             'date_end': date_end,
             'grouped_tasks': grouped_tasks,
-            'page_name': 'Hoto Task',
-            'default_url': '/hoto/tasks',
+            'page_name': 'milestone_task',
+            'default_url': '/purchase/milestone/tasks',
             'pager': pager,
             'searchbar_sortings': searchbar_sortings,
             'searchbar_groupby': searchbar_groupby,
@@ -154,10 +306,10 @@ class CustomerPortal(CustomerPortal):
             'searchbar_filters': OrderedDict(sorted(searchbar_filters.items())),
             'filterby': filterby,
         })
-        return request.render("de_portal_hoto_project.portal_hoto_site_tasks", values)
+        return request.render("de_portal_purchase_milestone.portal_purchase_milestone_tasks", values)
 
-    @http.route(['/hoto/task/<int:task_id>'], type='http', auth="public", website=True)
-    def portal_hoto_task(self, task_id, access_token=None, **kw):
+    @http.route(['/purchase/milestone/task/<int:task_id>'], type='http', auth="public", website=True)
+    def portal_purchase_milestone_task(self, task_id, access_token=None, **kw):
         try:
             task_sudo = self._document_check_access('project.task', task_id, access_token)
         except (AccessError, MissingError):
@@ -166,5 +318,5 @@ class CustomerPortal(CustomerPortal):
         # ensure attachment are accessible with access token inside template
         for attachment in task_sudo.attachment_ids:
             attachment.generate_access_token()
-        values = self._hoto_site_task_get_page_view_values(task_sudo, access_token, **kw)
-        return request.render("de_portal_hoto_project.portal_hoto_task", values)
+        values = self._milestone_task_get_page_view_values(task_sudo, access_token, **kw)
+        return request.render("de_portal_purchase_milestone.portal_purchase_milestone_task", values)
